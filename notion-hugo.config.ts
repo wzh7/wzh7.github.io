62771ff4d01e1a8aa8dbbf79dd3a654ce47c77a0
W.ZeHao <97936205+wzh7@users.noreply.github.com>
2023-09-26 11:09:41 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/wzh7/Github-Pages-Blog-c9972dd6d68149eb98dbc0fef288d5d7?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
